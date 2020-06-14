f515d28d6fe52c95129fad3efa28cf25f291350f
madubashena89 <madubashena89@gmail.com>
2020-06-14 17:53:30 -0500

    refactored code to Employee

--- src/main/java/Dearlership.java
+++ src/main/java/Dearlership.java
@@ -31,15 +31,16 @@ public class Dearlership {
             finance = true;
             System.out.println("What is your credit score?");
             int creditScore = sc.nextInt();
-            createCustomerAndVehicle(name, address, 0.0, true, creditScore, year, vehicleModel, price);
+            callEmployeeForHelp(name, address, 0.0, finance, creditScore, year, vehicleModel, price);
             System.out.println("Your transaction is processing !!");
             System.out.println("Hurray !!, thanks for the purchase. Here is your vehicle ");
 
         }
         else{
+            finance =false;
             System.out.println("How much money you have?");
             cashInHand = sc.nextDouble();
-            createCustomerAndVehicle(name, address, cashInHand, true, 850, year, vehicleModel, price);
+            callEmployeeForHelp(name, address, cashInHand, finance, 850, year, vehicleModel, price);
             System.out.println("Your transaction is processing !!");
             System.out.println("Hurray !!, thanks for the purchase. Here is your vehicle ");
 
@@ -47,96 +48,10 @@ public class Dearlership {
 
     }
 
-    public static void createCustomerAndVehicle(String name, String custAddress, double cashInHand, boolean finance,
-                                                int creditScore , int year, String model, int price){
-        Customer newCustomer = new Customer();
-        newCustomer.setName(name);
-        newCustomer.setCustomerAddress(custAddress);
-        newCustomer.setFinance(finance);
-        newCustomer.setCreditScore(creditScore);
-        newCustomer.setCashInHand(cashInHand);
-
-        Vehicle newVehicle = new Vehicle();
-        newVehicle.setYear(year);
-        newVehicle.setModel(model);
-        newVehicle.setPrice(price);
-        handleCustomer(newCustomer,newCustomer.isFinance(), newVehicle);
+    public static void callEmployeeForHelp(String name, String custAddress, double cashInHand, boolean finance,
+                                    int creditScore , int year, String model, int price){
+        Employee employeeName = new Employee();
+        employeeName.createCustomerAndVehicle(name, custAddress, cashInHand, finance, creditScore, year, model, price);
     }
 
-    public static void handleCustomer(Customer customer, boolean finance, Vehicle vehicle){
-        if(customer.isFinance()) {
-            assignFinanceToTheBank(customer,vehicle);
-        }
-        else if(vehicle.getPrice() <= customer.getCashInHand()){
-            processCashTransaction(customer, vehicle);
-        }
-        else {
-            System.out.println("Please bring this much money");
-        }
-    }
-
-    private static double getCreditScore(Customer customer){
-        double interestRate = 0.0;
-        if(customer.getCreditScore() >= 720){
-            interestRate = 0;
-        }else if(customer.getCreditScore() >= 650 && customer.getCreditScore() <720){
-            interestRate = 0.05;
-        }else if(customer.getCreditScore() >= 550 && customer.getCreditScore() <650){
-            interestRate = 0.15;
-        }else {
-            System.out.println(customer +" is not eligible to buy the car");
-        }
-        return interestRate;
-    }
-    
-    public static void processFinanceTransaction(Customer customer, Vehicle vehicle){
-        saveCustomerToList(customer,vehicle);
-        calculateTotalcarValueIfFinancing(customer, vehicle);
-    }
-
-    public static void assignFinanceToTheBank(Customer customer, Vehicle vehicle){
-        Double carValue =  calculateTotalcarValueIfFinancing(customer, vehicle);
-        List<Double> carValueList = new ArrayList<>();
-        carValueList.add(carValue);
-    }
-    
-    private static double calculateTotalcarValueIfFinancing(Customer customer, Vehicle vehicle){
-         double carValueWithoutInterestAndTax = vehicle.getPrice();
-         double taxRate = 0.075;
-         double totalInterest = getCreditScore(customer)*carValueWithoutInterestAndTax;
-         double totalvalueBeforeTax = carValueWithoutInterestAndTax + totalInterest;
-         double totalTax = totalvalueBeforeTax*taxRate;
-         double totalCarValueWithInterestAndTax = totalvalueBeforeTax + totalTax;
-         return  totalCarValueWithInterestAndTax;
-    }
-    
-    private static void processCashTransaction(Customer customer, Vehicle vehicle){
-            saveCustomerToList(customer,vehicle);
-            addCashToDearlershipAccount(customer,vehicle);
-    }
-    
-    private static String generateCustomerID(Customer customer, Vehicle vehicle){
-        String customerID =customer.getName()+ vehicle.getModel() + vehicle.getYear()+ customer.isFinance();
-        customer.setCustomerID(customerID);
-        return customerID;
-    }
-    
-    private static void saveCustomerToList(Customer customer, Vehicle vehicle){
-        List<String> customerList = new ArrayList<>();
-        customerList.add(generateCustomerID(customer, vehicle));
-    }
-    
-    public static void makeTransaction(Customer customer, Vehicle vehicle){
-        if(customer.isFinance()){
-            saveCustomerToList(customer,vehicle);
-            addCashToDearlershipAccount(customer,vehicle);
-        }
-    }
-    
-    public static void addCashToDearlershipAccount(Customer customer, Vehicle vehicle){
-        double dealrshipCash = 0;
-        while (dealrshipCash>0){
-            dealrshipCash += calculateTotalcarValueIfFinancing(customer, vehicle);
-        }
-    }
 }
--- src/main/java/Employee.java
+++ src/main/java/Employee.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Employee {
     private String name;
     private String employeeId;
@@ -54,4 +57,99 @@ public class Employee {
     public void setBranchName(String branchName) {
         this.branchName = branchName;
     }
+
+    public static void createCustomerAndVehicle(String name, String custAddress, double cashInHand, boolean finance,
+                                                int creditScore , int year, String model, int price){
+        Customer newCustomer = new Customer();
+        newCustomer.setName(name);
+        newCustomer.setCustomerAddress(custAddress);
+        newCustomer.setFinance(finance);
+        newCustomer.setCreditScore(creditScore);
+        newCustomer.setCashInHand(cashInHand);
+
+        Vehicle newVehicle = new Vehicle();
+        newVehicle.setYear(year);
+        newVehicle.setModel(model);
+        newVehicle.setPrice(price);
+        handleCustomer(newCustomer,newCustomer.isFinance(), newVehicle);
+    }
+
+    public static void handleCustomer(Customer customer, boolean finance, Vehicle vehicle){
+        if(customer.isFinance()) {
+            assignFinanceToTheBank(customer,vehicle);
+        }
+        else if(vehicle.getPrice() <= customer.getCashInHand()){
+            processCashTransaction(customer, vehicle);
+        }
+        else {
+            System.out.println("Please bring this much money");
+        }
+    }
+
+    private static double getCreditScore(Customer customer){
+        double interestRate = 0.0;
+        if(customer.getCreditScore() >= 720){
+            interestRate = 0;
+        }else if(customer.getCreditScore() >= 650 && customer.getCreditScore() <720){
+            interestRate = 0.05;
+        }else if(customer.getCreditScore() >= 550 && customer.getCreditScore() <650){
+            interestRate = 0.15;
+        }else {
+            System.out.println(customer +" is not eligible to buy the car");
+        }
+        return interestRate;
+    }
+
+    public static void processFinanceTransaction(Customer customer, Vehicle vehicle){
+        saveCustomerToList(customer,vehicle);
+        calculateTotalcarValueIfFinancing(customer, vehicle);
+    }
+
+    public static void assignFinanceToTheBank(Customer customer, Vehicle vehicle){
+        Double carValue =  calculateTotalcarValueIfFinancing(customer, vehicle);
+        List<Double> carValueList = new ArrayList<>();
+        carValueList.add(carValue);
+    }
+
+    private static double calculateTotalcarValueIfFinancing(Customer customer, Vehicle vehicle){
+        double carValueWithoutInterestAndTax = vehicle.getPrice();
+        double taxRate = 0.075;
+        double totalInterest = getCreditScore(customer)*carValueWithoutInterestAndTax;
+        double totalvalueBeforeTax = carValueWithoutInterestAndTax + totalInterest;
+        double totalTax = totalvalueBeforeTax*taxRate;
+        double totalCarValueWithInterestAndTax = totalvalueBeforeTax + totalTax;
+        return  totalCarValueWithInterestAndTax;
+    }
+
+    private static void processCashTransaction(Customer customer, Vehicle vehicle){
+        saveCustomerToList(customer,vehicle);
+        addCashToDearlershipAccount(customer,vehicle);
+    }
+
+    private static String generateCustomerID(Customer customer, Vehicle vehicle){
+        String customerID =customer.getName()+ vehicle.getModel() + vehicle.getYear()+ customer.isFinance();
+        customer.setCustomerID(customerID);
+        return customerID;
+    }
+
+    private static void saveCustomerToList(Customer customer, Vehicle vehicle){
+        List<String> customerList = new ArrayList<>();
+        customerList.add(generateCustomerID(customer, vehicle));
+    }
+
+    public static void makeTransaction(Customer customer, Vehicle vehicle){
+        if(customer.isFinance()){
+            saveCustomerToList(customer,vehicle);
+            addCashToDearlershipAccount(customer,vehicle);
+        }
+    }
+
+    public static void addCashToDearlershipAccount(Customer customer, Vehicle vehicle){
+        double dealrshipCash = 0;
+        while (dealrshipCash>0){
+            dealrshipCash += calculateTotalcarValueIfFinancing(customer, vehicle);
+        }
+    }
+
+
 }
